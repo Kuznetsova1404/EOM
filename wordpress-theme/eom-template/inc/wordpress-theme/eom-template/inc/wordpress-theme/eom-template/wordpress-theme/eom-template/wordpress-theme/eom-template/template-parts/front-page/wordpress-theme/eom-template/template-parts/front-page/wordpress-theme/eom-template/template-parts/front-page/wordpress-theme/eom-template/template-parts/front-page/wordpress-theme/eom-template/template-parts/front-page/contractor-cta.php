380dd0e4a3782f25a53b274839a4e169f2a28949
Kuznetsova1404 <91259691+Kuznetsova1404@users.noreply.github.com>
2026-04-07 17:08:52 +0500

    Create contractor-cta.php

---
+++ wordpress-theme/eom-template/inc/wordpress-theme/eom-template/inc/wordpress-theme/eom-template/wordpress-theme/eom-template/wordpress-theme/eom-template/template-parts/front-page/wordpress-theme/eom-template/template-parts/front-page/wordpress-theme/eom-template/template-parts/front-page/wordpress-theme/eom-template/template-parts/front-page/wordpress-theme/eom-template/template-parts/front-page/contractor-cta.php
@@ -0,0 +1,9 @@
+<section class="section">
+  <div class="container">
+    <div class="contractor-cta" id="contractors">
+      <h2><?php echo eom_section_title('contractor_cta_title', 'Вы event-подрядчик или агентство?'); ?></h2>
+      <p><?php echo esc_html((string) eom_acf_field('contractor_cta_text', 'Станьте нашим партнером и участвуйте в государственных проектах.')); ?></p>
+      <a class="btn btn--outline-light" href="#contacts">Стать партнером</a>
+    </div>
+  </div>
+</section>
