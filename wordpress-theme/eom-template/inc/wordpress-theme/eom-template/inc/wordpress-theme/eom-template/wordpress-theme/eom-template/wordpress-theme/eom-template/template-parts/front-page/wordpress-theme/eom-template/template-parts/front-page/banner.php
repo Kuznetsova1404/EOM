26528f0cf75317be45464c59945bdfb4aad9e50b
Kuznetsova1404 <91259691+Kuznetsova1404@users.noreply.github.com>
2026-04-07 17:07:33 +0500

    Create banner.php

---
+++ wordpress-theme/eom-template/inc/wordpress-theme/eom-template/inc/wordpress-theme/eom-template/wordpress-theme/eom-template/wordpress-theme/eom-template/template-parts/front-page/wordpress-theme/eom-template/template-parts/front-page/banner.php
@@ -0,0 +1,5 @@
+<section class="section section--tight">
+  <div class="container">
+    <div class="promo-banner"><?php echo esc_html((string) eom_acf_field('promo_banner_text', 'Любая сложность и масштаб. Работаем по всей России, странам ЕАЭС, MENA.')); ?></div>
+  </div>
+</section>
